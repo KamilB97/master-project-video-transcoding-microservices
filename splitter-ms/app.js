6e2af9b7eda63e1800cd3f51add871a77d682303
Kamil Brzycki <kbrzycki@gmail.com>
2021-04-02 21:45:36 -0700

    added constant paths to original video storage and chunks video storage

--- splitter-ms/app.js
+++ splitter-ms/app.js
@@ -1,7 +1,9 @@
 const VideoManager = require('./videoManager')
 const fileName = 'original.mp4';
 
+const FILE_STORAGE = 'E:\\workspace\\Videos\\1280x720\\full\\mp4\\';
+const CHUNK_STORAGE = 'E:\\workspace\\Videos\\1280x720\\chunks\\mp4\\';
 
 
-const videoManager = new VideoManager();
+const videoManager = new VideoManager(FILE_STORAGE, CHUNK_STORAGE);
 videoManager.splitVideoToChunks(fileName);
